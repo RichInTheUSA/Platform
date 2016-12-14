4b56e06bffb7140567af9636c12878be80807daa
RichInTheUSA <invalid_account@verizon.net>
2016-12-13 20:24:24 -0500

    Java Tutorial Trail Platform

--- src/Platform/JavaTutorialTrailPlatform.java
+++ src/Platform/JavaTutorialTrailPlatform.java
@@ -53,10 +53,35 @@ public class JavaTutorialTrailPlatform {
         }
         try {
             nP.load(i);
+            if ( i != null) {
+                i.close();
+            }
         } catch (IOException ex) {
-            //
+            Logger.getLogger(JavaTutorialTrailPlatform.class.getName()).log(Level.SEVERE, null, ex);
         }
+    
         nP.list(System.out);
+        
+        
+        // Command line arguments
+        for (String s : args) {
+            System.out.println (s);
+        }
+        
+        System.out.println(System.getProperty("file.seperator"));
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("java.home"));
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("line.separator"));
+        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(System.getProperty("path.seperator"));
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.name"));
         
     }
     
